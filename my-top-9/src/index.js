e6ae63275c7a5591baba1392b811f89de4749e9c
joshua-disney <joshua.a.disney@gmail.com>
2019-02-07 09:55:07 -0700

    Began working on get user by id.

--- my-top-9/src/index.js
+++ my-top-9/src/index.js
@@ -5,11 +5,13 @@ import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import axios from 'axios'
 
 import rootReducer from './store/reducers';
 import App from './App';
 
 import './index.css';
+axios.default.withCredentials = true
 
 const store = createStore(
   rootReducer,
