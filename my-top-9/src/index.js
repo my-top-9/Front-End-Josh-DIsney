9eba7454df182f7b7f7a270200fe46304b7aad65
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-02-07 18:55:35 -0500

    test

--- my-top-9/src/index.js
+++ my-top-9/src/index.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import axios from 'axios'
+import React from "react";
+import ReactDOM from "react-dom";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
 
-import rootReducer from './store/reducers';
-import App from './App';
+import rootReducer from "./store/reducers";
+import App from "./App";
 
-import './index.css';
-axios.default.withCredentials = true
+import "./index.css";
+axios.default.withCredentials = true;
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk, logger)
-);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
-  <Provider store ={store}>
+  <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </Provider>, 
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
