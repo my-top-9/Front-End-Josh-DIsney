bc0f6640690c615ef8776ea74a7022af1d74eecf
Brian Ruff <thelonewolf198927@gmail.com>
2019-02-07 19:04:39 -0500

    fix bootstrap import error

--- my-top-9/src/index.js
+++ my-top-9/src/index.js
@@ -5,7 +5,6 @@ import logger from "redux-logger";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
 import axios from "axios";
 
 import rootReducer from "./store/reducers";
